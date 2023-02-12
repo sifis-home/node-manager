f5918de9579d8cf41fcab0aa65624b798348cc98
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-02-12 23:08:22 +0100

    Add tests

---
+++ tests/node-manager.rs
@@ -0,0 +1,42 @@
+use node_manager::{self, NodeManager};
+use rsa::pkcs8::DecodePrivateKey;
+
+fn make_node_manager() -> NodeManager {
+    use rsa::pkcs8::EncodePrivateKey;
+
+    let local_key =
+        rsa::RsaPrivateKey::from_pkcs8_pem(include_str!("../tests/test_key1.pem")).unwrap();
+    let local_key_der = local_key.to_pkcs8_der().unwrap();
+
+    NodeManager::new(local_key_der.as_ref())
+}
+
+#[test]
+fn node_manager_test_basic() {
+    let _mgr = make_node_manager();
+}
+
+#[test]
+fn node_manager_test_signing() {
+    let test_key_1 = rsa::RsaPrivateKey::from_pkcs8_pem(include_str!("test_key1.pem")).unwrap();
+    let test_key_2 = rsa::RsaPrivateKey::from_pkcs8_pem(include_str!("test_key2.pem")).unwrap();
+
+    let test_op = node_manager::Operation::AddByAdmin(vec![1, 2, 3, 4]);
+    let test_op_other = node_manager::Operation::AddByAdmin(vec![4, 3, 2, 1]);
+
+    let msg = test_op.clone().sign(246_802_000, &test_key_1).unwrap();
+
+    assert!(msg.signature_is_valid(&test_key_1));
+
+    // Test that validation fails with different key
+    assert!(!msg.signature_is_valid(&test_key_2));
+
+    // Test that validation fails with different data for time
+    let mut msg_diff_time = msg.clone();
+    msg_diff_time.timestamp += 1;
+    assert!(!msg_diff_time.signature_is_valid(&test_key_1));
+
+    let mut msg_diff_time = msg.clone();
+    msg_diff_time.timestamp += 1;
+    assert!(!msg_diff_time.signature_is_valid(&test_key_1));
+}
