3d4978d56228d4993ab71bfab6589fcc0ebb9cfb
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-02-13 01:35:19 +0100

    Fix node_manager_test_signing warnings

--- tests/node-manager.rs
+++ tests/node-manager.rs
@@ -1,18 +1,18 @@
-use node_manager::{self, NodeId, NodeManager};
+use node_manager::{self, NodeManager};
 use rsa::pkcs8::DecodePrivateKey;
 
 fn make_node_manager() -> NodeManager {
     use rsa::pkcs8::EncodePrivateKey;
 
-    fn gen_fn(data: &[u8]) -> Result<NodeId, ()> {
-        Ok(NodeId::from_data(data))
+    fn gen_fn(data: &[u8]) -> Result<Vec<u8>, ()> {
+        Ok(data.to_vec())
     }
 
     let local_key =
         rsa::RsaPrivateKey::from_pkcs8_pem(include_str!("../tests/test_key1.pem")).unwrap();
     let local_key_der = local_key.to_pkcs8_der().unwrap();
 
-    NodeManager::new(gen_fn, local_key_der.as_ref())
+    NodeManager::new(local_key_der.as_ref(), gen_fn)
 }
 
 #[test]
@@ -26,7 +26,6 @@ fn node_manager_test_signing() {
     let test_key_2 = rsa::RsaPrivateKey::from_pkcs8_pem(include_str!("test_key2.pem")).unwrap();
 
     let test_op = node_manager::Operation::AddByAdmin(vec![1, 2, 3, 4]);
-    let test_op_other = node_manager::Operation::AddByAdmin(vec![4, 3, 2, 1]);
 
     let msg = test_op.clone().sign(246_802_000, &test_key_1).unwrap();
 
@@ -36,10 +35,6 @@ fn node_manager_test_signing() {
     assert!(!msg.signature_is_valid(&test_key_2));
 
     // Test that validation fails with different data for time
-    let mut msg_diff_time = msg.clone();
-    msg_diff_time.timestamp += 1;
-    assert!(!msg_diff_time.signature_is_valid(&test_key_1));
-
     let mut msg_diff_time = msg.clone();
     msg_diff_time.timestamp += 1;
     assert!(!msg_diff_time.signature_is_valid(&test_key_1));
