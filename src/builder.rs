067951664991c96a4c75ad03763099267b48198b
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-02-23 15:54:16 +0100

    Add node itself to node table
    This node table is given to (re-)joining nodes,
    so while a node obviously knows about itself,
    the table will also be given to others.

--- src/builder.rs
+++ src/builder.rs
@@ -1,4 +1,6 @@
-use crate::{ManagerState, NodeIdGenerator, NodeManager, SHARED_KEY_LEN};
+use crate::{
+    ManagerState, NodeEntry, NodeId, NodeIdGenerator, NodeManager, NodeStatus, SHARED_KEY_LEN,
+};
 use rsa::pkcs8::{DecodePrivateKey, EncodePublicKey};
 use rsa::RsaPrivateKey;
 use std::collections::HashMap;
@@ -36,15 +38,30 @@ impl NodeManagerBuilder {
         let local_peer_id = PeerId::from(local_key_pair.public());
         local_peer_id.to_bytes();*/
         let key_pub = key_pair.to_public_key().to_public_key_der().unwrap();
+        let public_key_der = key_pub.as_ref().to_vec();
 
         let node_id = node_id_generator(key_pub.as_ref()).unwrap();
 
-        let (shared_key, state) = if let Some(k) = shared_key {
-            (k, ManagerState::WaitingForKey)
+        let mut nodes = HashMap::new();
+        let (shared_key, state, self_status) = if let Some(k) = shared_key {
+            (k, ManagerState::MemberOkay, NodeStatus::Member)
         } else {
-            // TODO we need to do more than just wait for MemberOkay
-            (Vec::new(), ManagerState::MemberOkay)
+            (
+                Vec::new(),
+                ManagerState::WaitingForKey,
+                NodeStatus::WaitingEntry,
+            )
         };
+
+        nodes.insert(
+            NodeId::from_data(&node_id),
+            NodeEntry {
+                public_key: key_pair.to_public_key(),
+                public_key_der,
+                status: self_status,
+                last_seen_time: super::timestamp(),
+            },
+        );
 
         if ![0, SHARED_KEY_LEN].contains(&shared_key.len()) {
             panic!("Invalid length for shared key: {}", shared_key.len());
@@ -56,7 +73,7 @@ impl NodeManagerBuilder {
             shared_key,
             node_id,
             admin_keys: Vec::new(),
-            nodes: HashMap::new(),
+            nodes,
             state,
             vote_proposal: None,
         }
