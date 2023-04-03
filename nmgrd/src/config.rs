a4e615da823edd0fe0ebce0ad3ad09735cddfef9
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-04-03 18:35:25 +0200

    admin key loading

--- nmgrd/src/config.rs
+++ nmgrd/src/config.rs
@@ -13,10 +13,25 @@ impl Config {
         if self.admin_key.is_none() && self.admin_key_path.is_none() {
             errs.push("Neither admin_key nor admin_key_path specified".to_owned());
         }
+        if self.admin_key.is_some() && self.admin_key_path.is_some() {
+            errs.push(
+                "Both admin_key and admin_key_path specified, please specify only one".to_owned(),
+            );
+        }
         if errs.is_empty() {
             Ok(())
         } else {
             Err(errs)
         }
+    }
+    pub fn admin_key(&self) -> Result<String, anyhow::Error> {
+        if let Some(admin_key) = &self.admin_key {
+            return Ok(admin_key.clone());
+        }
+        if let Some(admin_key_path) = &self.admin_key_path {
+            let admin_key_str = std::fs::read_to_string(admin_key_path)?;
+            return Ok(admin_key_str);
+        }
+        panic!("Invalid config: admin_key or admin_key_path required.");
     }
 }
