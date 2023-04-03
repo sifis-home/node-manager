ab4477b65283ef7b78e8a2f4768f3e90746d195b
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-04-03 19:02:45 +0200

    Also load the private key

--- nmgrd/src/config.rs
+++ nmgrd/src/config.rs
@@ -5,6 +5,8 @@ pub struct Config {
     addr_dht: String,
     admin_key_path: Option<String>,
     admin_key: Option<String>,
+    priv_key_path: Option<String>,
+    priv_key: Option<String>,
 }
 
 impl Config {
@@ -16,6 +18,14 @@ impl Config {
         if self.admin_key.is_some() && self.admin_key_path.is_some() {
             errs.push(
                 "Both admin_key and admin_key_path specified, please specify only one".to_owned(),
+            );
+        }
+        if self.priv_key.is_none() && self.priv_key_path.is_none() {
+            errs.push("Neither priv_key nor priv_key_path specified".to_owned());
+        }
+        if self.priv_key.is_some() && self.priv_key_path.is_some() {
+            errs.push(
+                "Both priv_key and priv_key_path specified, please specify only one".to_owned(),
             );
         }
         if errs.is_empty() {
@@ -34,6 +44,16 @@ impl Config {
         }
         panic!("Invalid config: admin_key or admin_key_path required.");
     }
+    pub fn priv_key(&self) -> Result<String, anyhow::Error> {
+        if let Some(priv_key) = &self.priv_key {
+            return Ok(priv_key.clone());
+        }
+        if let Some(priv_key_path) = &self.priv_key_path {
+            let priv_key_str = std::fs::read_to_string(priv_key_path)?;
+            return Ok(priv_key_str);
+        }
+        panic!("Invalid config: priv_key or priv_key_path required.");
+    }
 }
 
 #[cfg(test)]
@@ -46,6 +66,7 @@ mod test {
         let st = r#"
             addr_dht = "Hi"
             admin_key_path = "/path/to/admin-pub-key.pem"
+            priv_key_path = "/path/to/admin-pub-key.pem"
         "#;
         let cfg: Config = from_str(st).unwrap();
         cfg.validate().unwrap();
@@ -55,10 +76,29 @@ mod test {
     fn test_loading_invalid() {
         let st = r#"
             addr_dht = "Hi"
-            # missing admin_key_path
+            #admin_key_path = "/path/to/admin-pub-key.pem"
+            priv_key_path = "/path/to/admin-pub-key.pem"
         "#;
         let cfg: Config = from_str(st).unwrap();
         let errs = cfg.validate().unwrap_err();
         assert_eq!(errs.len(), 1);
+
+        let st = r#"
+            addr_dht = "Hi"
+            admin_key_path = "/path/to/admin-pub-key.pem"
+            #priv_key_path = "/path/to/admin-pub-key.pem"
+        "#;
+        let cfg: Config = from_str(st).unwrap();
+        let errs = cfg.validate().unwrap_err();
+        assert_eq!(errs.len(), 1);
+
+        let st = r#"
+            addr_dht = "Hi"
+            #admin_key_path = "/path/to/admin-pub-key.pem"
+            #priv_key_path = "/path/to/admin-pub-key.pem"
+        "#;
+        let cfg: Config = from_str(st).unwrap();
+        let errs = cfg.validate().unwrap_err();
+        assert_eq!(errs.len(), 2);
     }
 }
