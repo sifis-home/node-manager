848b9d087639d27ec9f7ceaaf5e01fb65de326bb
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-04-16 07:37:28 +0200

    Don't use leak() functions at all
    Seems that LeakSanitizer is smarter than we thought, mere pointers aren't enough...

--- node-manager/tests/ed25519.rs
+++ node-manager/tests/ed25519.rs
@@ -21,9 +21,10 @@ impl RngCore for TestRng {
 
 impl CryptoRng for TestRng {}
 
-// This static exists so that the LeakSanitizer doesn't think the keys are "leaked"
+// These statics exist so that the LeakSanitizer doesn't think the keys are "leaked"
 // TODO: once once_cell from core stabilizes, use that one
-static KEYS: OnceCell<&'static [&'static str]> = OnceCell::new();
+static KEYS_STORAGE: OnceCell<Vec<Box<str>>> = OnceCell::new();
+static KEYS: OnceCell<Vec<&'static str>> = OnceCell::new();
 
 fn test_key() -> &'static str {
     test_keys()[0]
@@ -31,18 +32,24 @@ fn test_key() -> &'static str {
 
 fn test_keys() -> &'static [&'static str] {
     KEYS.get_or_init(|| {
-        let mut rng = TestRng(SmallRng::seed_from_u64(10_424_143));
-        (0..20)
-            .map(|_| {
-                &*Box::leak(
+        let vec_ref = KEYS_STORAGE.get_or_init(|| {
+            let mut rng = TestRng(SmallRng::seed_from_u64(10_424_143));
+            (0..20)
+                .map(|_| {
                     PrivateKey::generate_ed25519_rng(&mut rng)
                         .to_pkcs8_pem()
                         .unwrap()
-                        .into_boxed_str(),
-                )
+                        .into_boxed_str()
+                })
+                .collect::<Vec<_>>()
+        });
+        vec_ref
+            .iter()
+            .map(|r| {
+                let r: &'static str = r;
+                r
             })
-            .collect::<Vec<_>>()
-            .leak()
+            .collect::<Vec<&'static str>>()
     })
 }
 
