60d3ff9b2bc676f989adcf03d8c53f6110d64b70
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-04-27 15:12:14 +0200

    Add example that adds test config.toml files

---
+++ nmgrd/examples/make-test-files.rs
@@ -0,0 +1,58 @@
+use anyhow::Result;
+use base64ct::{Base64, Encoding};
+use node_manager::admin::AdminNode;
+use node_manager::keys::PrivateKey;
+use node_manager::timestamp;
+use serde::Serialize;
+use std::fs::write as write_file;
+use std::str::FromStr;
+
+#[derive(Serialize)]
+struct Config {
+    dht_url: String,
+    admin_key: String,
+    priv_key: String,
+    admin_join_msg: String,
+    lobby_key: String,
+    shared_key: Option<String>,
+    lobby_loopback_only: bool,
+}
+
+const INIT_SHARED_KEY: &str = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
+
+fn make_test_files(count: u16) -> Result<()> {
+    let admin_key = PrivateKey::generate_ed25519();
+    let admin_key_der = admin_key.to_pkcs8_der().unwrap();
+    let admin_key_public_pem = admin_key.to_public_key().to_pkcs8_pem().unwrap();
+    let admin_node = AdminNode::from_key_pair_der(&admin_key_der);
+    for i in 0..count {
+        let priv_key = PrivateKey::generate_ed25519();
+        let node_key_pub_der = priv_key.to_public_key().to_public_key_der().unwrap();
+        let admin_join_msg_buf = admin_node
+            .sign_addition(&node_key_pub_der, timestamp().unwrap())
+            .unwrap()
+            .serialize();
+        let admin_join_msg = Base64::encode_string(&admin_join_msg_buf);
+        let shared_key = (i == 0).then(|| INIT_SHARED_KEY.to_string());
+        let cfg = Config {
+            dht_url: "https://127.0.0.1:3000/".into(),
+            admin_key: admin_key_public_pem.clone(),
+            priv_key: priv_key.to_pkcs8_pem().unwrap(),
+            admin_join_msg,
+            lobby_key: "ff0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1fffff".into(),
+            shared_key,
+            lobby_loopback_only: true,
+        };
+        let cfg_toml = toml::to_string(&cfg)?;
+        write_file(format!("config-{i:02}.toml"), cfg_toml)?;
+    }
+    Ok(())
+}
+
+fn main() {
+    let count = std::env::args()
+        .next()
+        .and_then(|n_str| u16::from_str(&n_str).ok())
+        .unwrap_or(5);
+    make_test_files(count).unwrap();
+}
