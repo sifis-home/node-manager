1fb755c1529cf4631aa52c6aca4337086b011a9c
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-05-02 01:01:48 +0200

    Add error context to file error

--- nmgrd/src/main.rs
+++ nmgrd/src/main.rs
@@ -1,6 +1,6 @@
 use crate::config::Config;
 use crate::context::Context;
-use anyhow::{bail, Error};
+use anyhow::{bail, Context as _, Error};
 
 mod config;
 mod context;
@@ -20,7 +20,8 @@ fn load_config() -> Result<(Config, String), Error> {
     let Some(cfg_file_path) = std::env::args().nth(1) else {
         bail!("Please specify path to config.toml on the command line");
     };
-    let cfg_file_str = std::fs::read_to_string(&cfg_file_path)?;
+    let cfg_file_str = std::fs::read_to_string(&cfg_file_path)
+        .with_context(|| format!("Failed to read config file from {cfg_file_path}"))?;
     let cfg: Config = toml::from_str(&cfg_file_str)?;
     if let Err(errs) = cfg.validate() {
         let errs_string = errs.join("\n");
@@ -32,8 +33,6 @@ fn load_config() -> Result<(Config, String), Error> {
 #[allow(unused)]
 async fn run(cfg: Config, cfg_path: &str) -> Result<(), Error> {
     let mut ctx = Context::start(cfg, cfg_path).await?;
-
-    ctx.broadcast_admin_join_msg().await?;
 
     // main loop
     loop {
