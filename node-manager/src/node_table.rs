6277cafea8ecc3437583460ad4cc39b0a16ba922
Arpad Müller <arpad.mueller@iit.cnr.it>
2023-05-26 13:04:09 +0200

    Make increasing/decreasing preserve the precondition in certain settings
    This makes the precondition be preserved that all three scalars sum up to 1.0,
    and also are inside [0.0, 1.0], in situatons like:
    * when increasing belief: and uncertaintiy or disbelief is smaller than delta/2.0:
      ensuring that uncertainity and disbelief stay above 0.0
    * when increasing belief: when belief is already so close to 1.0 that adding delta
      puts us above: ensuring that this doesn't happen
    * the analog of the two points above for when increasing disbelief
    * the analog of the second point for increasing uncertainty
    
    In theory we could also normalize the vector with the L1 or L2 norm,
    but this will mean a non linear behaviour, and that "true" extremes
    like 0.0 or 1.0 are never reached unless the difference gets smaller
    than float precision.

--- node-manager/src/node_table.rs
+++ node-manager/src/node_table.rs
@@ -34,20 +34,63 @@ impl Default for JosangTrust {
     }
 }
 
+fn inc_val(delta: f64, v_to_inc: &mut f64, v1: &mut f64, v2: &mut f64) {
+    let delta = delta.min(1.0 - *v_to_inc);
+    *v_to_inc += delta;
+    let dh = delta / 2.0;
+    match (*v1 >= dh, *v2 >= dh) {
+        (true, true) => {
+            *v1 -= dh;
+            *v2 -= dh;
+        }
+        (true, false) => {
+            let v2_val = *v2;
+            *v2 = 0.0;
+            // TODO this should not make v1 be smaller than 0.0, but maybe add an assertion?
+            *v1 -= delta - v2_val;
+        }
+        (false, true) => {
+            let v1_val = *v1;
+            *v1 = 0.0;
+            // TODO this should not make v2 be smaller than 0.0, but maybe add an assertion?
+            *v2 -= delta - v1_val;
+        }
+        (false, false) => {
+            // This actually shouldn't happen, as we ensured that delta <= (1.0 - *v_to_inc)
+            // which as per precondition of the vector being L1-normal is equal to v1 + v2.
+            // TODO print an error or something
+        }
+    }
+    // TODO this isn't it: it reduces too little if one of the two values are below
+    let max_reduce = (delta / 2.0).min(v1.max(*v2));
+    *v1 = (*v1 - max_reduce).max(0.0);
+    *v2 -= (*v2 - max_reduce).max(0.0);
+}
+
 impl JosangTrust {
     pub fn inc_belief(&mut self, d_b: f64) {
-        self.belief += d_b;
-        self.uncertainty -= d_b / 2.0;
-        self.disbelief -= d_b / 2.0;
+        inc_val(
+            d_b,
+            &mut self.belief,
+            &mut self.uncertainty,
+            &mut self.disbelief,
+        );
     }
     pub fn inc_disbelief(&mut self, d_d: f64) {
-        self.disbelief += d_d;
-        self.uncertainty -= d_d / 2.0;
-        self.belief -= d_d / 2.0;
+        inc_val(
+            d_d,
+            &mut self.disbelief,
+            &mut self.uncertainty,
+            &mut self.belief,
+        );
     }
     pub fn inc_uncertainty(&mut self, d_u: f64) {
+        let d_u = d_u.min(1.0 - self.uncertainty);
         self.belief -= d_u;
         self.uncertainty += d_u;
+    }
+    pub fn reputation(&self) -> f64 {
+        self.belief - self.disbelief - self.uncertainty
     }
 }
 
